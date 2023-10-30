62d0e6c0ca831e3fc7fdba6aed73146af4816ec9
artur-almgren <149424069+artur-almgren@users.noreply.github.com>
2023-10-30 15:34:10 -0300

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 from PIL import Image
 
 
-import plotly.graph_objects as go
+#import plotly.graph_objects as go
 from streamlit_option_menu import option_menu
 #from gtspython import tonGrad
 st.set_page_config(layout="wide")
