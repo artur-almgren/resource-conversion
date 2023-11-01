5c47377ea12929232674d2b6e5bdc829c1d3c520
artur-almgren <149424069+artur-almgren@users.noreply.github.com>
2023-10-31 21:13:47 -0300

    Update resource-conversion.py

--- resource-conversion.py
+++ resource-conversion.py
@@ -5,9 +5,6 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import plotly.graph_objects as go
-from streamlit_option_menu import option_menu
-
-#from streamlit_option_menu import option_menu
 
 #from gtspython import tonGrad
 st.set_page_config(layout="wide")
